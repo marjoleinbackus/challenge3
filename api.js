bde0808bdc9a6fde40ca17fbb669ce702c57b30e
marjoleinbackus <marjoleinbackus@gmail.com>
2016-11-29 17:41:41 +0100

    I made my page but API doesn't work anymore. What went wrong?

--- api.js
+++ api.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
       search_kyoto()
       search_sapporo()
       search_fukuoka()
+      search_coastal()
+      search_nature()
+      search_villages()
+      search_cities()
     })
 
 function search_tokyo() {
@@ -98,3 +102,99 @@ function handleResults_fukuoka(response_data) {
     image.appendTo("#fukuoka")
   })
 }
+
+  // duplicate again for another city
+
+function search_coastal() {
+  $.ajax({
+    url: 'https://pixabay.com/api/',
+    method: 'GET',
+    data: {
+      q: "dutch+coastal+area",
+      key: '2409187-ee958c2d8895c6eb14d3ce314',
+      per_page: "3"
+    },
+    success: handleResults_coastal
+  })
+}
+
+function handleResults_cities(response_data) {
+  response_data.hits.forEach(function(item) {
+    var url = item.webformatURL
+    var image = $(document.createElement('img'));
+    image.attr('src', url)
+    image.appendTo("#coastal")
+  })
+
+// duplicate again for another city
+
+function search_nature() {
+  $.ajax({
+    url: 'https://pixabay.com/api/',
+    method: 'GET',
+    data: {
+      q: "dutch+nature",
+      key: '2409187-ee958c2d8895c6eb14d3ce314',
+      per_page: "3"
+    },
+    success: handleResults_nature
+  })
+}
+
+function handleResults_nature(response_data) {
+  response_data.hits.forEach(function(item) {
+    var url = item.webformatURL
+    var image = $(document.createElement('img'));
+    image.attr('src', url)
+    image.appendTo("#nature")
+  })
+
+  // duplicate again for another city
+
+function search_villages() {
+  $.ajax({
+    url: 'https://pixabay.com/api/',
+    method: 'GET',
+    data: {
+      q: "dutch+villages",
+      key: '2409187-ee958c2d8895c6eb14d3ce314',
+      per_page: "3"
+    },
+    success: handleResults_villages
+  })
+}
+
+function handleResults_villages(response_data) {
+  response_data.hits.forEach(function(item) {
+    var url = item.webformatURL
+    var image = $(document.createElement('img'));
+    image.attr('src', url)
+    image.appendTo("#villages")
+  })
+
+  // duplicate again for another city
+
+function search_cities() {
+  $.ajax({
+    url: 'https://pixabay.com/api/',
+    method: 'GET',
+    data: {
+      q: "dutch+cities",
+      key: '2409187-ee958c2d8895c6eb14d3ce314',
+      per_page: "3"
+    },
+    success: handleResults_cities
+  })
+}
+
+function handleResults_cities(response_data) {
+  response_data.hits.forEach(function(item) {
+    var url = item.webformatURL
+    var image = $(document.createElement('img'));
+    image.attr('src', url)
+    image.appendTo("#cities")
+  })
+
+
+
+
